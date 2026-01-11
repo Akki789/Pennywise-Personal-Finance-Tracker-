e419859087eb9344ea44d75b64367836905b5080
Akash <aakashkumarag96@gmail.com>
2026-01-11 19:50:35 +0530

    Designed the cards using ant design

---
+++ pennywise/src/components/Cards/Index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./styles.css";
+import { Card, Row } from "antd";
+import Button from "../Button/Index";
+
+export default function Cards() {
+  return (
+    <div>
+      <Row className="my-row">
+        <Card bordered={true} className="my-card">
+          <h2>Current Balance</h2>
+          <p>₹60000</p>
+          <Button text="Reset Balance" blue={true} />
+        </Card>
+
+        <Card bordered={true} className="my-card">
+          <h2>Total Income</h2>
+          <p>₹1200000</p>
+          <Button text="Add Income" blue={true} />
+        </Card>
+
+        <Card bordered={true} className="my-card">
+          <h2>Total Expenses</h2>
+          <p>₹300000</p>
+          <Button text="Add Expenses" blue={true} />
+        </Card>
+      </Row>
+    </div>
+  );
+}
--- pennywise/src/pages/Dashboard.jsx
+++ pennywise/src/pages/Dashboard.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
-import Header from '../components/Header/Index'
+import React from "react";
+import Header from "../components/Header/Index";
+import Cards from "../components/Cards/Index";
 
 export default function Dashboard() {
   return (
-    <div><Header /></div>
-  )
+    <div>
+      <Header />
+      <Cards />
+    </div>
+  );
 }
