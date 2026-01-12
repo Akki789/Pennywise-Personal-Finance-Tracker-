9309ec3c743fcd6afc620629f13d2246ce27412f
Akash <aakashkumarag96@gmail.com>
2026-01-12 17:18:38 +0530

    Completed Modals components

--- pennywise/src/components/Cards/Index.jsx
+++ pennywise/src/components/Cards/Index.jsx
@@ -3,7 +3,7 @@ import "./styles.css";
 import { Card, Row } from "antd";
 import Button from "../Button/Index";
 
-export default function Cards() {
+export default function Cards({ showExpenseModal, showIncomeModal }) {
   return (
     <div>
       <Row className="my-row">
@@ -16,13 +16,13 @@ export default function Cards() {
         <Card bordered={true} className="my-card">
           <h2>Total Income</h2>
           <p>₹1200000</p>
-          <Button text="Add Income" blue={true} />
+          <Button text="Add Income" blue={true} onClick={showIncomeModal}/>
         </Card>
 
         <Card bordered={true} className="my-card">
           <h2>Total Expenses</h2>
           <p>₹300000</p>
-          <Button text="Add Expenses" blue={true} />
+          <Button text="Add Expenses" blue={true} onClick={showExpenseModal} />
         </Card>
       </Row>
     </div>
--- pennywise/src/pages/Dashboard.jsx
+++ pennywise/src/pages/Dashboard.jsx
@@ -1,12 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header/Index";
 import Cards from "../components/Cards/Index";
+import AddExpense from "../components/Modals/AddExpense";
+import AddIncome from "../components/Modals/AddIncome";
 
 export default function Dashboard() {
+  
+  const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
+  const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
+ 
+
+  const showExpenseModal = () => {
+    
+    setIsExpenseModalVisible(true);
+  };
+
+  const showIncomeModal = () => {
+   
+    setIsIncomeModalVisible(true);
+  };
+
+  const handleExpenseCancel = () => {
+    setIsExpenseModalVisible(false);
+  };
+
+  const handleIncomeCancel = () => {
+    setIsIncomeModalVisible(false);
+  };
+
+  const onFinish = (values, type) => {
+    console.log("on finish", values, type);
+  };
+
   return (
     <div>
       <Header />
-      <Cards />
+
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <Cards
+            showExpenseModal={showExpenseModal}
+            showIncomeModal={showIncomeModal}
+          />
+
+          <AddExpense
+           
+            isExpenseModalVisible={isExpenseModalVisible}
+            handleExpenseCancel={handleExpenseCancel}
+            onFinish={onFinish}
+          />
+
+          <AddIncome
+            isIncomeModalVisible={isIncomeModalVisible}
+            handleIncomeCancel={handleIncomeCancel}
+            onFinish={onFinish}
+          />
+
+          
+        </>
+      )}
     </div>
   );
 }
