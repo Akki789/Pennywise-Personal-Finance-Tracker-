3fb0610b3a38ab20d82e440e442d3cfe9463af93
Akash <aakashkumarag96@gmail.com>
2026-01-12 19:35:17 +0530

    Completed searching and sorting

--- pennywise/src/components/TransactionsTable/TransactionsTable.jsx
+++ pennywise/src/components/TransactionsTable/TransactionsTable.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import './styles.css';
-import { Table } from 'antd';
+import React, { useState } from "react";
+import "./styles.css";
+import { Radio, Select, Table } from "antd";
+import searchImg from "../../assets/search.svg";
 
-export default function TransactionsTable({transactions}) {
+export default function TransactionsTable({ transactions }) {
+  const [search, setSearch] = useState("");
+  const [typeFilter, setTypeFilter] = useState("");
+  const [sortKey, setSortKey] = useState("");
 
-    const columns = [
+  const columns = [
     {
       title: "Name",
       dataIndex: "name",
@@ -32,5 +36,105 @@ export default function TransactionsTable({transactions}) {
     },
   ];
 
-  return <Table dataSource={transactions} columns={columns} />
+  let filteredTransactions = transactions.filter(
+    (item) =>
+      item.name.toLowerCase().includes(search.toLowerCase()) &&
+      item.type.includes(typeFilter)
+  );
+
+  let sortedTransactions = filteredTransactions.sort((a, b) => {
+    if (sortKey === "date") {
+      return new Date(a.date) - new Date(b.date);
+    } else if (sortKey === "amount") {
+      return a.amount - b.amount;
+    } else {
+      return 0;
+    }
+  });
+
+  return (
+    <div
+      style={{
+        width: "95%",
+        padding: "0rem 2rem",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          gap: "1rem",
+          alignItems: "center",
+          marginBottom: "1rem",
+        }}
+      >
+        <div className="input-flex">
+          <img src={searchImg} width="16" />
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search by name"
+          />
+        </div>
+
+        <Select
+          className="select-input"
+          onChange={(value) => setTypeFilter(value)}
+          value={typeFilter}
+          placeholder="Filter"
+          allowClear
+        >
+          <Select.Option value="">All</Select.Option>
+          <Select.Option value="income">Income</Select.Option>
+          <Select.Option value="expense">Expense</Select.Option>
+        </Select>
+      </div>
+
+      <div className="my-table">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+            marginBottom: "1rem",
+          }}
+        >
+          <h2>My Transactions</h2>
+
+          <Radio.Group
+            className="input-radio"
+            onChange={(e) => setSortKey(e.target.value)}
+            value={sortKey}
+          >
+            <Radio.Button value="">No Sort</Radio.Button>
+            <Radio.Button value="date">Sort by Date</Radio.Button>
+            <Radio.Button value="amount">Sort by Amount</Radio.Button>
+          </Radio.Group>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              gap: "1rem",
+              width: "400px",
+            }}
+          >
+            <button className="btn">Export to CSV</button>
+            <label for="file-csv" className="btn btn-blue">
+              Import from CSV
+            </label>
+            <input
+              id="file-csv"
+              type="file"
+              accept=".csv"
+              required
+              style={{ display: "none" }}
+            />
+          </div>
+        </div>
+
+        <Table dataSource={sortedTransactions} columns={columns} />
+      </div>
+    </div>
+  );
 }
