3d7573e944605e47ae63021e8d29fd5ffb2b1b27
Akash <aakashkumarag96@gmail.com>
2026-01-12 18:31:04 +0530

    All the transactions part completed

--- pennywise/src/components/Cards/Index.jsx
+++ pennywise/src/components/Cards/Index.jsx
@@ -3,25 +3,31 @@ import "./styles.css";
 import { Card, Row } from "antd";
 import Button from "../Button/Index";
 
-export default function Cards({ showExpenseModal, showIncomeModal }) {
+export default function Cards({
+  income,
+  expenses,
+  totalBalance,
+  showExpenseModal,
+  showIncomeModal,
+}) {
   return (
     <div>
       <Row className="my-row">
         <Card bordered={true} className="my-card">
           <h2>Current Balance</h2>
-          <p>₹60000</p>
+          <p>₹{totalBalance}</p>
           <Button text="Reset Balance" blue={true} />
         </Card>
 
         <Card bordered={true} className="my-card">
           <h2>Total Income</h2>
-          <p>₹1200000</p>
-          <Button text="Add Income" blue={true} onClick={showIncomeModal}/>
+          <p>₹{income}</p>
+          <Button text="Add Income" blue={true} onClick={showIncomeModal} />
         </Card>
 
         <Card bordered={true} className="my-card">
           <h2>Total Expenses</h2>
-          <p>₹300000</p>
+          <p>₹{expenses}</p>
           <Button text="Add Expenses" blue={true} onClick={showExpenseModal} />
         </Card>
       </Row>
--- pennywise/src/pages/Dashboard.jsx
+++ pennywise/src/pages/Dashboard.jsx
@@ -1,23 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header/Index";
 import Cards from "../components/Cards/Index";
 import AddExpense from "../components/Modals/AddExpense";
+import moment from "moment";
 import AddIncome from "../components/Modals/AddIncome";
+import { addDoc, collection, getDocs, query } from 'firebase/firestore';
+import { auth, db } from '../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
 
 export default function Dashboard() {
-  
+  const [transactions, setTransactions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [user] = useAuthState(auth);
   const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
   const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
-  const [loading, setLoading] = useState(false);
- 
+  const [income, setIncome] = useState(0);
+  const [expenses, setExpenses] = useState(0);
+  const [totalBalance, setTotalBalance] = useState(0);
+  
+
 
   const showExpenseModal = () => {
-    
     setIsExpenseModalVisible(true);
   };
 
   const showIncomeModal = () => {
-   
     setIsIncomeModalVisible(true);
   };
 
@@ -28,10 +36,87 @@ export default function Dashboard() {
   const handleIncomeCancel = () => {
     setIsIncomeModalVisible(false);
   };
-
+  
+ 
   const onFinish = (values, type) => {
-    console.log("on finish", values, type);
+    const newTransaction = {
+      type: type,
+      date: moment(values.date).format("YYYY-MM-DD"),
+      amount: parseFloat(values.amount),
+      tag: values.tag,
+      name: values.name,
+    };
+
+    addTransaction(newTransaction);
+
+    setIsExpenseModalVisible(false);
+    setIsIncomeModalVisible(false);
+
   };
+
+ async function addTransaction(transaction, many) {
+    try {
+      const docRef = await addDoc(
+        collection(db, `users/${user.uid}/transactions`),
+        transaction
+      );
+      console.log("Document written with ID: ", docRef.id);
+      if (!many) toast.success("Transaction Added!");
+      let newArr = transactions;
+      newArr.push(transaction);
+      setTransactions(newArr);
+      calculateBalance();
+    } catch (e) {
+      console.error("Error adding document: ", e);
+      if (!many) toast.error("Couldn't add transaction");
+      
+    }
+  }
+
+useEffect(() => {
+  // Get all docs from a collection
+  if (user) fetchTransactions();
+}, [user]);
+
+
+  function calculateBalance(){
+    let incomeTotal = 0;
+    let expensesTotal = 0;
+
+    transactions.forEach((transaction) => {
+      if (transaction.type === "income") {
+        incomeTotal += transaction.amount;
+      } else {
+        expensesTotal += transaction.amount;
+      }
+    });
+
+    setIncome(incomeTotal);
+    setExpenses(expensesTotal);
+    setTotalBalance(incomeTotal - expensesTotal);
+  }
+
+  // Calculate the initial balance, income, and expenses if transactions changes
+  useEffect(() => {
+    calculateBalance();
+  }, [transactions]);
+
+  async function fetchTransactions() {
+    setLoading(true);
+    if (user) {
+      const q = query(collection(db, `users/${user.uid}/transactions`));
+      const querySnapshot = await getDocs(q);
+      let transactionsArray = [];
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        transactionsArray.push(doc.data());
+      });
+      setTransactions(transactionsArray);
+      // console.log("Transactions Array", transactionsArray)
+      toast.success("Transactions Fetched!");
+    }
+    setLoading(false);
+  }
 
   return (
     <div>
@@ -42,12 +127,14 @@ export default function Dashboard() {
       ) : (
         <>
           <Cards
+            totalBalance={totalBalance}
+            income={income}
+            expenses={expenses}
             showExpenseModal={showExpenseModal}
             showIncomeModal={showIncomeModal}
           />
 
           <AddExpense
-           
             isExpenseModalVisible={isExpenseModalVisible}
             handleExpenseCancel={handleExpenseCancel}
             onFinish={onFinish}
@@ -58,9 +145,8 @@ export default function Dashboard() {
             handleIncomeCancel={handleIncomeCancel}
             onFinish={onFinish}
           />
-
-          
         </>
+      
       )}
     </div>
   );
